ee97b3abe800e286f0a6d828399a865ee2ba71bd
Saurav <sp.gharti@gmail.com>
2018-03-07 22:15:44 +0000

    add package aa_stripe

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'aa_stripe', 'url': 'http://pypi.python.org/pypi/aa_stripe/0.4.1'},
 	{'name':'django-paypal2', 'url': 'http://pypi.python.org/pypi/django-paypal2/1.0.2'},
 	{'name':'django-shop-paypal', 'url': 'http://pypi.python.org/pypi/django-shop-paypal/0.0.1'},
 	{'name':'djangoshop-paypal', 'url': 'http://pypi.python.org/pypi/djangoshop-paypal/0.2.1'},
