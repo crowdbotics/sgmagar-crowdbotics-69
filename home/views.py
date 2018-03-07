d7d1087c80fcda8ef53c90ca0dd1f91f02bce4a2
Saurav <sp.gharti@gmail.com>
2018-03-07 22:15:49 +0000

    add package dj-paypal

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'dj-paypal', 'url': 'http://pypi.python.org/pypi/dj-paypal/0.6.0'},
 	{'name':'aa_stripe', 'url': 'http://pypi.python.org/pypi/aa_stripe/0.4.1'},
 	{'name':'django-paypal2', 'url': 'http://pypi.python.org/pypi/django-paypal2/1.0.2'},
 	{'name':'django-shop-paypal', 'url': 'http://pypi.python.org/pypi/django-shop-paypal/0.0.1'},
